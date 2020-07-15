814866cd1a855b43b81776a20756f374e72db01d
Mei <1261369154@qq.com>
2020-07-15 21:19:15 +0800

    配置默认样式

---
+++ thepetshop-app/src/components/AppHead/index.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+
+class AppHead extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <header>
+                顶部
+            </header>
+        )
+    }
+}	
+export default AppHead;
--- thepetshop-app/src/index.js
+++ thepetshop-app/src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, BrowserRouter } from 'react-router-dom';
-import './index.css';
+import './assets/reset.css'
 import App from './App';
 
-const Router = process.env.NODE_ENV === 'production' ? BrowserRouter : HashRouter;
-
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>
-  ,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
-
