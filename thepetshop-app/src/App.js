fac37a74735dd5e614850fb865d014b95fbf6062
bigbig <1421786744@qq.com>
2020-07-17 14:44:38 +0800

    整合路由

--- thepetshop-app/src/App.js
+++ thepetshop-app/src/App.js
@@ -1,37 +1,19 @@
 import React, { Component } from 'react';
 // import 'antd/dist/antd.css';
-import { withRouter, Route } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import 'lib-flexible';
 import 'antd-mobile/dist/antd-mobile.css';
 import '@/assets/icon/iconfont.css'
 
 // components
-// import AppHead from './components/AppHead';
-import AppMain from './components/AppMain';
-import AppFooter from './components/AppFooter';
-import Login from '@/views/Login';
-import Reg from '@/views/Reg';
+import Layout from '@/views/Layout';
 
 @withRouter
 class App extends Component {
   render() {
-    // console.log(this.props);
-    const { location: { pathname } } = this.props;
     return (
       <div className="App">
-        {
-          pathname === '/login' || pathname ==='/reg' ? (
-            <>
-              <Route path='/login' component={Login} />
-              <Route path='/reg' component={Reg} />
-            </>
-          ) : (
-            <>
-              <AppMain />
-              <AppFooter />
-            </>
-          )
-        }
+          <Layout/>
       </div>
     );
   }
--- thepetshop-app/src/api/goods/index.js
+++ thepetshop-app/src/api/goods/index.js
@@ -1,13 +1,12 @@
 import request from '@/utils/request.js';
 import {host1,host2} from '@/config.json';
 
-// const url = "http://192.168.0.105:3099";
 
 export default{
     randomGoods(num) {
         return request({
             method:'get',
-            url:host1+'/goods/randomgoods',
+            url:host2+'/goods/randomgoods',
             params:{
                 num
             }
