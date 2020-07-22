2f55ae2c03a9a12b88057d4f802701a3985cde6d
Mei <1261369154@qq.com>
2020-07-22 12:01:44 +0800

    添加全局loading效果，添加返回顶部，修复一些bug

--- thepetshop-app/src/utils/request.js
+++ thepetshop-app/src/utils/request.js
@@ -1,7 +1,25 @@
 import axios from 'axios';
-
+import { Toast } from 'antd-mobile';
 const request = axios.create({
     baseURL:process.env.REACT_APP_SERVICE_URL,//REACT_APP_SERVICE_URL
     timeout: 5000,
+})
+
+request.interceptors.request.use(config=>{
+    Toast.loading('加载中...', 30, () => {
+        console.log("???");
+    });
+    return config
+}, error=>{
+    Toast.hide();
+    return error
+})
+
+request.interceptors.response.use(response=>{
+    Toast.hide();
+    return response;
+},error=>{
+    Toast.hide();
+    return Promise.reject(error);
 })
 export default request;
